146aa01f2400a753b39121dc556dc8fe05126010
Divine-77777 <dynamicphillic77777@gmail.com>
2026-01-17 22:24:27 +0530

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import IntroVideo from "./components/IntroVideo";
+import CareerOverview from "./components/CareerOverview";
 
 import Stats from "./components/Stats";
 import WorkExperience from "./components/WorkExperience";
@@ -10,6 +11,7 @@ import AboutUs from "./components/AboutUs";
 import Footer from "./components/Footer";
 import Achievements from "./components/Achievements";
 import Gallery from "./components/Gallery";
+import Memories from "./components/Memories";
 
 const Page = () => {
   return (
@@ -17,9 +19,11 @@ const Page = () => {
       <Navbar />
       <Home />
       <IntroVideo />
+      <CareerOverview />
 
       <div className="flex flex-col gap-0 py-20 pb-0">
         <Stats />
+        <Memories />
         <WorkExperience />
         <Achievements />
         <Gallery />
